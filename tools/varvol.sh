00e6173d21242257154da72e95f2b7b1d626ed26
Kui <kui@Kui.(none)>
2014-12-08 19:50:54 +0800

    add script to remove varvol and merge the space to rootvol

---
+++ tools/varvol.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# remove varvol
+varvol=`lvdisplay   |grep  varvol |grep "LV Path"  | awk '{print $3}'`
+lvremove  $varvol
+
+# extend rootvol
+free_pe=`vgdisplay   |grep "Free  PE"  |awk '{print $5}'`
+rootvol=`lvdisplay   |grep  rootvol |grep "LV Path"  | awk '{print $3}'`
+current_pe=`lvdisplay  $rootvol |grep "Current LE" |awk '{print $3}'`
+echo $free_pe
+echo $current_pe
+new_pe=`expr $free_pe + $current_pe`
+lvextend -l $new_pe $rootvol
+
+
+lvdisplay
